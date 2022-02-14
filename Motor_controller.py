e6b26630af547988c3197d012147d08c4922b54f
MDkontroller <nicosarmientom@gmail.com>
2022-02-14 12:24:58 +0100

    Motor Steuerung

--- Motor_controller.py
+++ Motor_controller.py
@@ -1,23 +1,30 @@
 #! /usr/bin/env python3
 
 import RPi.GPIO as GPIO
+import time
 from time import sleep
 
-GPIO.setmode(GPIO.BOARD) 
-GPIO.setup(12,GPIO.OUT)
+
+GPIO.setmode(GPIO.BMC) 
+GPIO.setup(17,GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(27,GPIO.OUT,initial=GPIO.LOW)
+GPIO.setup(22,GPIO.OUT,initial=GPIO.LOW)
+#GPIO.setup(26,GPIO.OUT,initial=GPIO.LOW)
+GPIO.cleanup()
 
 
+time = time.time()
 
-while True:
+while (time-(time.time()< 5)):
 
 # SET HIGH PIN
 
-    GPIO.output(12,1)
+    GPIO.output(17,1)
+    GPIO.output(22,1)
+    GPIO.output(27,1)
 
-    sleep(0.5)
+    sleep(0.1)
 
-# SET LOW
-
-    GPIO.output(12,0)
-
-    sleep(0.5)
+    GPIO.output(17,0)
+    GPIO.output(22,0)
+    GPIO.output(27,0)
